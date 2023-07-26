be6c91324eff7e5b16f7f8d84ac4a39ebe96c457
Suraj0156 <130788586+Suraj0156@users.noreply.github.com>
2023-07-26 11:47:28 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your JS code here. If required.
-it("should have the pulse animation applied", () => {
-  cy.get(".box").should(($box) => {
-    const animation = $box.css("animation");
-    expect(animation).to.include("pulse");
-  });
-});
